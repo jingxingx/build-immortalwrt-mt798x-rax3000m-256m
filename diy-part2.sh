a965066158b26a6b8a0ea1fd59f08952cfec41e2
jingxingx <yyxwan-2006@163.com>
2024-12-19 13:59:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,8 +17,8 @@ sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_genera
 #sed -i 's/ImmortalWrt/rax3000m_256m/g' package/base-files/files/bin/config_generate
 
 #修改wifi名称（mtwifi-cfg）
-sed -i 's/ASUS_33003/OpenWrt/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
-sed -i 's/ASUS_33003_5G/OpenWrt5G/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i 's/ASUS_33003/ImmortalWrt-2.4G/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i 's/ASUS_33003_5G/ImmortalWrt-5G/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
 #修改闪存为256M版本(这是针对原厂128闪存来的，但又要编译256M固件来的）
 #sed -i 's/<0x580000 0x7200000>/<0x580000 0xee00000>/g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-cmcc-rax3000m.dts
