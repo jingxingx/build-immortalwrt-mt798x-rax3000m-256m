643ab02f7baca7137fc8f32a8759233024a37b0e
jingxingx <yyxwan-2006@163.com>
2024-12-24 13:48:41 +0800

    Update diy-part1_1.sh

--- diy-part1_1.sh
+++ diy-part1_1.sh
@@ -13,7 +13,8 @@
 # Uncomment a feed source
 
 #删除可能冲突的包
-rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
+#rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
+#rm -rf feeds/luci/applications/luci-app-mosdns && rm -rf feeds/packages/net/{alist,adguardhome,smartdns}
 rm -rf feeds/luci/applications/luci-app-mosdns && rm -rf feeds/packages/net/{alist,adguardhome,smartdns}
 
 rm -rf feeds/packages/lang/golang
